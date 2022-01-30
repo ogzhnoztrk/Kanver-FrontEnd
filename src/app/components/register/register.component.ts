5174ef211c416574f6f6dc036653a8df7fec2899
oguzhanoztrk00@gmail.com <oguzhanoztrk00@gmail.com>
2022-01-30 12:32:35 +0300

    300122:1232

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  result: any;
+  user = new User();
 
-  constructor() { }
+  constructor(private userService: UserService
+     ) {}
 
   ngOnInit(): void {
+   
+    this.user.name = 'Oğuzhan';
+    this.user.lastName = 'Öztürk';
+    this.user.birthDay = 2000;
+    this.user.bloodTypeId = 2;
+    this.user.email = 'user7@example.com';
+    this.user.password = '123';
+    this.user.identityNumber = "37018041374";
+    this.userRegister();
   }
+
+  
+  userRegister() {
+    
+    this.userService.registerUser(this.user).subscribe(resp =>{
+      console.log(resp)
+    });
+  }
+
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,20 +4,46 @@ import { User } from '../models/user/user';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  apiUrl = 'https://localhost:44366';
 
-  apiUrl = "https://localhost:44366"
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient:HttpClient) { }
-
-  loginUser(mail:string,password:string):Observable<ListResponseModel<User>>{
-    let newPath = this.apiUrl+'/Login?mail='+ mail +'&password=' + password;
+  loginUser(
+    mail: string,
+    password: string
+  ): Observable<ListResponseModel<User>> {
+    let newPath = this.apiUrl + '/Login?mail=' + mail + '&password=' + password;
     return this.httpClient.get<ListResponseModel<User>>(newPath);
-  }  
+  }
 
-  registerUser(user:User):Observable<ListResponseModel<User>>{
-      return this.httpClient.post<ListResponseModel<User>>(this.apiUrl+"postUser", user);
+  registerUser(user: User): Observable<ListResponseModel<User>> {
+    let newPath =
+      this.apiUrl +
+      'userId=' +
+      user.userId +
+      '&bloodTypeId=' +
+      user.bloodTypeId +
+      '&name=' +
+      user.name +
+      '&lastName=' +
+      user.lastName +
+      '&email=' +
+      user.email +
+      '&phoneNumber=' +
+      user.phoneNumber +
+      '&password=' +
+      user.password +
+      '&isValidate=' +
+      user.isValidate +
+      '&isMernisOk=' +
+      user.isMernisOk +
+      '&identityNumber=' +
+      user.identityNumber +
+      '&birthDay=' +
+      user.birthDay;
+    return this.httpClient.post<ListResponseModel<User>>(this.apiUrl, user);
   }
 }
