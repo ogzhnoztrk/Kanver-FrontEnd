71e845cf9a9d78a6b5a2e6e624249891abaff5da
cihangemi <83552885+cihangemi@users.noreply.github.com>
2022-01-30 14:01:22 +0300

    sıjı

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FaqService } from 'src/app/services/faq.service';
 
 @Component({
   selector: 'app-faq',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
+  result: any;
 
-  constructor() { }
+  constructor(private faqService: FaqService) { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
